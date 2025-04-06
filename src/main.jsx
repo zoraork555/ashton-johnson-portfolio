0230ff7fb27fc25790c9e765dea786fa18285bd5
zoraork555 <acjohnson30@crimson.ua.edu>
2025-04-06 16:58:18 -0500

    Changed about path

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Projects from './pages/Projects'
-import About from './pages/about.jsx'
+import About from './pages/About.jsx'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 
