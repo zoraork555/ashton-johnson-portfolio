57e73dc23074a397aeecf80c1e0c26f0e89269eb
zoraork555 <acjohnson30@crimson.ua.edu>
2025-04-06 19:57:05 -0500

    added debug messages and changed scripts again

--- server.cjs
+++ server.cjs
@@ -1,17 +1,24 @@
 const express = require("express");
+console.log("HERE");
 const path = require('path');
+console.log("HERE");
 const app = express();
+console.log("HERE");
 
 const port = process.env.PORT || 3000;
+console.log("HERE");
 
 // Serve static files from dist
 app.use(express.static(path.join(__dirname, 'dist')));
+console.log("HERE");
 
 // For SPA (React Router)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
+console.log("HERE");
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+console.log("HERE");
